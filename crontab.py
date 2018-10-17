3d8749e3a9b6eb9fe5930ab7a8eb835345571b94
kungfudong <43160715+kongfudong@users.noreply.github.com>
2018-10-17 17:05:56 +0800

    Add files via upload
    add log to output log.txt

--- crontab.py
+++ crontab.py
@@ -3,8 +3,18 @@ import datetime
 import sys
 import logging
 
-logging.basicConfig(level = logging.INFO,format ='%(asctime)s - %(name)s - %(levelname)s : %(message)s')
 logger =logging.getLogger(__name__)
+logger.setLevel(level = logging.INFO)
+handler = logging.FileHandler("crontab_log.txt")
+handler.setLevel(logging.INFO)
+formatter=logging.Formatter('%(asctime)s - %(name)s - %(levelname)s : %(message)s')
+handler.setFormatter(formatter)
+
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+
+logger.addHandler(handler)
+logger.addHandler(console)
 
 #sys.setrecursionlimit(5000)
 
